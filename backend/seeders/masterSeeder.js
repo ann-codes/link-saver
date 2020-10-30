16f78deceec9e4128484210c6615b27208d0ba56
Ann <ann.nguyen.codes@gmail.com>
2020-10-29 22:40:27 -0400

    complete working seed file, run upon deployment

--- backend/seeders/masterSeeder.js
+++ backend/seeders/masterSeeder.js
@@ -1,3 +1,4 @@
+// const mongoose = require("mongoose");
 const Blog = require("../models/blog");
 const User = require("../models/user");
 const createUsers = require("./createUsers");
@@ -7,12 +8,17 @@ const masterSeeder = async () => {
   console.log("[ Running seeder ]");
 
   try {
+    console.log("[ Deleting users ]");
+    // await User.deleteMany({}); /// fails here, when running npm run seed, why?
+    // delete only seems to work in app.js file
     await createUsers();
   } catch (e) {
     console.log("ERROR CREATING USERS ==>", e);
   }
 
   try {
+    console.log("[ Deleting links ]");
+    await Blog.deleteMany({}); // seeding fails at delete for npm run seed
     await createLinks();
   } catch (e) {
     console.log("ERROR CREATING LINKS ==>", e);
